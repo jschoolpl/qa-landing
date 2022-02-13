53c50b3b736558bf5bc23f59bc0195a2fbe76a45
e1r0nd <a.merezhanyi@gmail.com>
2022-02-13 08:49:56 +0100

    done with the markup

--- src/script.js
+++ src/script.js
@@ -1,12 +1,45 @@
-// some comments
+function toggleVisibility(e) {
+  const target = e.target;
 
-function helloWorld(str) {
-    if (str == 'world') {
-        return 'Hello world!';
-    } else {
-        return 'Hello ' + str + '!';
-    }
+  if (target.classList.contains("js-toggle-button")) {
+    const parent = target.parentElement;
+    const content = parent.querySelector(".js-toggle-content");
+    const isVisible = content.getAttribute("data-visible") === "true";
+    const appendix = target.querySelector(".js-toggle-appendix");
+
+    isVisible
+      ? collapseSection(content, appendix)
+      : expandSection(content, appendix);
+  }
 }
 
-const greeting = helloWorld('world');
-console.log(greeting);
+document
+  .querySelector("#questions")
+  .addEventListener("click", toggleVisibility);
+
+function collapseSection(element, target) {
+  const sectionHeight = element.scrollHeight;
+  const elementTransition = element.style.transition;
+
+  element.style.transition = "";
+  requestAnimationFrame(() => {
+    element.style.height = `${sectionHeight}px`;
+    element.style.transition = elementTransition;
+    requestAnimationFrame(() => (element.style.height = "0px"));
+  });
+
+  target.innerText = "+";
+  element.setAttribute("data-visible", "false");
+}
+
+function expandSection(element, target) {
+  const sectionHeight = element.scrollHeight;
+
+  element.style.height = sectionHeight + "px";
+  element.addEventListener("transitionend", (e) =>
+    element.removeEventListener("transitionend", arguments.callee)
+  );
+
+  target.innerText = "-";
+  element.setAttribute("data-visible", "true");
+}
